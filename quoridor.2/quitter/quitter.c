c450eba1970563c9ca651a0a2bbb8e38c006a4ea
theo-crn <theophile.carayon@edu.ece.fr>
2024-11-24 23:05:09 +0100

    Update quitter.c

--- quoridor.2/quitter/quitter.c
+++ quoridor.2/quitter/quitter.c
@@ -3,25 +3,8 @@
 #include "../sauvegarde/sauvegarde.h"
 
 void quitter(int *pquitte) {
-    char strch;
-
-    printf("\t\tEntrer 'q' pour quitter la partie\n");
-
-    // Utilisez un moyen sûr pour vider le buffer, si nécessaire.
-    while (getchar() != '\n'); // Vider le buffer d'entrée avant de lire.
-
-    scanf("%c", &strch);
-
-    if (strch == 'q') {
-        printf("fin\n");
-        printf("%d\n", *pquitte);
-        *pquitte = 1;
-        printf("%d\n", *pquitte);
-        return;
-    }
-
-    printf("pas fin\n");
-    printf("%d\n", *pquitte);
-    *pquitte = 0;
-    printf("%d\n", *pquitte);
+    printf("\t\tEntrer '0' pour quitter la partie\n");
+    fflush(stdin);
+    scanf("%d", pquitte);
+    printf("%d", *pquitte);
 }
