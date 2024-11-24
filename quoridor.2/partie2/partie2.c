1693e33150ae9937e8ecb4b59b4bf72e6f83588c
Adrien-Botin <adrien.botin@edu.ece.fr>
2024-11-24 15:51:54 +0100

    dossier partie2
    partie2 appel des fonctions

---
+++ quoridor.2/partie2/partie2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "partie2.h"
+#include "../gagner/gagner.h"
+#include "../move/move.h"
+#include "../plateau/plateau.h"
+#include "../barriere/barriere.h"
+#include "../affichage/affichage.h"
+
+
+void partie2(char piona[3],char pionb[3]) {
+
+}
---
+++ quoridor.2/partie2/partie2.h
@@ -0,0 +1,7 @@
+
+#ifndef PARTIE2_H
+#define PARTIE2_H
+
+void partie2(char piona[3],char pionb[3]);
+
+#endif //PARTIE2_H
