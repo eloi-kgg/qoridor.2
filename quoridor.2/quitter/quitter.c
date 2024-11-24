74cef34ab5b1fb8fcd179adfe5b7edf4ef8dbcdf
theo-crn <theophile.carayon@edu.ece.fr>
2024-11-24 22:58:16 +0100

    quitter4

--- quoridor.2/quitter/quitter.c
+++ quoridor.2/quitter/quitter.c
@@ -4,20 +4,24 @@
 
 void quitter(int *pquitte) {
     char strch;
+
     printf("\t\tEntrer 'q' pour quitter la partie\n");
-    fflush(stdin);
+
+    // Utilisez un moyen sûr pour vider le buffer, si nécessaire.
+    while (getchar() != '\n'); // Vider le buffer d'entrée avant de lire.
+
     scanf("%c", &strch);
-    if (strch == 'q')
-    {
+
+    if (strch == 'q') {
         printf("fin\n");
-        printf("%d\n",*pquitte);
+        printf("%d\n", *pquitte);
         *pquitte = 1;
-        printf("%d\n",*pquitte);
+        printf("%d\n", *pquitte);
         return;
     }
+
     printf("pas fin\n");
-    printf("%d\n",*pquitte);
+    printf("%d\n", *pquitte);
     *pquitte = 0;
-    printf("%d\n",*pquitte);
-    return;
+    printf("%d\n", *pquitte);
 }
